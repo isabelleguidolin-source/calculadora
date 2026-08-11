778ae76242bc4fc30b33e0ea9dca071850ed2575
isabelleguidolin-source <isabelle.guidolin@escola.pr.gov.br>
2026-08-11 08:12:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let nome;
+let numero1;
+let numero2;
+let soma;
+let subtraçao;
+let divisao;
+let multiplicaçao;
+
+nome = prompt("Qual é o seu nome?");
+
+alert("Hello, eja bem vindo" + nome );
+
+numero1= Number (prompt (" digite o primeiro numero"));
+numero2= Number (prompt ("digite o segundo numero"));
+
+
+soma = numero1 + numero2;
+subtraçao = numero1 - numero2;
+divisao = numero1 / numero2;
+multiplicaçao = numero1 * numero2;
+
+
+alert ("resultados: \n soma = " + soma + " \n subtracao = " + subtraçao + "\n divisao =" + divisao + "\n multiplicaçao =" + multiplicaçao);
